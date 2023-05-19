04a90673d19d0a2e117102605ce198bd97a3c61d
cristiano-woody <cristiano.nog.woody@gmail.com>
2023-05-19 16:20:37 -0300

    refactor: Adding tab navigation and new screens

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
+import Tasks from './src/pages/Tasks'
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import CreateTask from './src/pages/CreateTask';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer >
+      <Tab.Navigator  screenOptions={{
+          tabBarActiveTintColor: '#50DD99', // Cor ativa do item selecionado
+          tabBarInactiveTintColor: 'gray', // Cor dos itens não selecionados
+          headerStyle: {
+            backgroundColor: '#1B2128', // Cor de fundo do menu de navegação
+            borderBottomWidth: 1,
+            borderBottomColor: '#50DD99'
+          },
+          headerTitleStyle: {
+            color: '#FFFFFF'
+          },
+          tabBarStyle: {
+            backgroundColor: '#1B2128', // Cor de fundo do menu de navegação
+            borderTopColor: '#50DD99'
+          },
+        }}>
+        <Tab.Screen name="Tasks" component={Tasks} />
+        <Tab.Screen name="Adding" component={CreateTask} />
+        <Tab.Screen name= "Search" component={CreateTask}/>
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
