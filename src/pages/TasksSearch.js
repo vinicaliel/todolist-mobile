693d6fb026767fc60d67862fab1d237c07c0e129
Cristiano-woody <cristiano.nog.woody@gmail.com>
2023-06-01 21:43:27 -0300

    refactor:  Adding tasks search function

--- src/pages/TasksSearch.js
+++ src/pages/TasksSearch.js
@@ -1,25 +1,85 @@
-import { StyleSheet, View, FlatList, Text, TextInput, TouchableOpacity} from 'react-native';
+import { StyleSheet, View, Text, TextInput, TouchableOpacity, FlatList } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+import { useState, useEffect } from 'react';
+import CardTasks from '../components/CardTasks';
+import Axios from 'axios';
 
 const TasksSearch = () => {
-    return(
-        <View style= {styles.container}>
-            <TextInput
-              style={styles.inputSearch}
-              placeholder="Task title"
-              placeholderTextColor= "#9D9D9D" 
+    const [taskSearch, setTasksSearch] = useState([]);
+    const [titleTaskInput, setTitleTaskInput] = useState('');
+
+    const searchTask = () => {
+        try {
+            Axios.get(`http://192.168.0.10:3333/task/id/?id=${titleTaskInput}`)
+                .then((response) => {
+                    setTasksSearch([response.data]);
+                });
+
+        } catch (err) {
+            console.log(err.message);
+        }
+    };
+
+    //so para debugar
+    useEffect(() => {
+        console.log(taskSearch);
+    }, [taskSearch]);
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.containerSearch}>
+                <TextInput
+                    style={styles.inputSearch}
+                    placeholder="Task title"
+                    placeholderTextColor="#9D9D9D"
+                    onChangeText={(value) => setTitleTaskInput(value)}
+                />
+                <TouchableOpacity style={styles.buttonSearch} onPress={searchTask}>
+                    <FontAwesome name="search" size={24} color="black" />
+                </TouchableOpacity>
+            </View>
+            <FlatList
+                style={styles.flatListTasks}
+                numColumns={1}
+                keyExtractor={(item) => item.id}
+                data={taskSearch}
+                renderItem={({ item }) => (
+                    <CardTasks Title={item.title} Description={item.description} />
+                )}
             />
-            <TouchableOpacity>
-                <Text></Text>
-            </TouchableOpacity>
         </View>
-    )
-}
+    );
+};
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#1B2128'
+    },
+    inputSearch: {
+        padding: 5,
+        borderWidth: 1,
+        borderColor: '#50DD99',
+        borderRadius: 10,
+        width: 300,
+        color: '#FFFFFF'
+    },
+    containerSearch: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 10,
+        marginTop: 15,
+        marginBottom: 20
+    },
+    buttonSearch: {
+        width: 40,
+        height: 40,
+        backgroundColor: '#50DD99',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 5
     }
 });
 
-export default TasksSearch
+export default TasksSearch;
